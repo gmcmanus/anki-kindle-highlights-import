977bf44141e0f57246afd982c628af6dfed3d2c2
gmcmanus <gmcmanus@users.noreply.github.com>
2017-09-08 15:24:23 +0000

    Remember added time of last highlight

--- kindle_highlights/__init__.py
+++ kindle_highlights/__init__.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import re
 from collections import namedtuple
 
@@ -20,44 +21,47 @@ def import_highlights():
         clippings, bad_clippings = parse_clippings(file)
 
     if bad_clippings:
-        showText('The following clippings could not be parsed:\n\n' + '\n==========\n'.join(bad_clippings))
+        showText(
+            f'The following {len(bad_clippings)} clippings could not be parsed:\n\n' +
+            '\n==========\n'.join(bad_clippings))
 
-    notes_added = 0
+    config = mw.addonManager.getConfig(__name__)
 
-    for clipping in clippings:
-        if clipping.kind != 'Highlight':
-            continue
+    highlight_clippings = list(highlights_only(clippings))
+    clippings_to_add = after_last_added(highlight_clippings, last_added_datetime(config))
 
-        model = mw.col.models.byName('IR3')
+    model = mw.col.models.byName('IR3')
+
+    clipping = None
+
+    for clipping in clippings_to_add:
         note = Note(mw.col, model)
-
-        def fields():
-            for field in mw.col.models.fieldNames(model):
-                if field == 'Text':
-                    yield clipping.content
-                elif field == 'Title':
-                    yield '{kind} from {document}{page}{location} added {added}'.format(
-                        kind=clipping.kind,
-                        document=clipping.document,
-                        page=' page ' + clipping.page if clipping.page is not None else '',
-                        location=' location ' + clipping.location if clipping.location is not None else '',
-                        added=clipping.added,
-                    )
-                elif field == 'Source':
-                    yield '{kind} from {document}{page}{location}'.format(
-                        kind=clipping.kind,
-                        document=clipping.document,
-                        page=' page ' + clipping.page if clipping.page is not None else '',
-                        location=' location ' + clipping.location if clipping.location is not None else '',
-                    )
-                else:
-                    raise ValueError('Unknown field: ' + field)
-
-        note.fields = list(fields())
+        note.fields = list(fields(clipping, model))
         mw.col.addNote(note)
-        notes_added += 1
 
-    showInfo(f'{notes_added} notes imported')
+    if clipping:
+        config['last_added'] = added_datetime(clipping).isoformat()
+        mw.addonManager.writeConfig(__name__, config)
+
+    def info():
+        if clippings_to_add:
+            yield f'{len(clippings_to_add)} new highlights imported'
+
+        num_old_highlights = len(highlight_clippings) - len(clippings_to_add)
+        if num_old_highlights:
+            yield f'{num_old_highlights} old highlights ignored'
+
+        num_not_highlights = len(clippings) - len(highlight_clippings)
+        if num_not_highlights:
+            yield f'{num_not_highlights} non-highlight clippings ignored'
+
+    info_strings = list(info())
+    if info_strings:
+        showInfo(', '.join(info_strings) + '.')
+    elif bad_clippings:
+        showInfo('No other clippings found.')
+    else:
+        showInfo('No clippings found.')
 
 
 Clipping = namedtuple('Clipping', ('kind', 'document', 'page', 'location', 'added', 'content'))
@@ -101,6 +105,60 @@ CLIPPING_PATTERN = r'''\ufeff?(?P<document>.*)
 
 (?P<content>.*)
 ?'''
+
+
+def after_last_added(clippings, last_added):
+    if not last_added:
+        return clippings
+
+    def reversed_clippings_after_last_added():
+        for clipping in reversed(clippings):
+            clipping_added = added_datetime(clipping)
+            if clipping_added <= last_added:
+                return
+            yield clipping
+
+    clippings_after_last_added = list(reversed_clippings_after_last_added())
+    clippings_after_last_added.reverse()
+    return clippings_after_last_added
+
+
+def added_datetime(clipping):
+    return datetime.strptime(clipping.added, '%A, %B %d, %Y %I:%M:%S %p')
+
+
+def last_added_datetime(config):
+    last_added_config = config['last_added']
+    return datetime.strptime(last_added_config, '%Y-%m-%dT%H:%M:%S') if last_added_config else None
+
+
+def highlights_only(clippings):
+    for clipping in clippings:
+        if clipping.kind == 'Highlight':
+            yield clipping
+
+
+def fields(clipping, model):
+    for field in mw.col.models.fieldNames(model):
+        if field == 'Text':
+            yield clipping.content
+        elif field == 'Title':
+            yield '{kind} from {document}{page}{location} added {added}'.format(
+                kind=clipping.kind,
+                document=clipping.document,
+                page=' page ' + clipping.page if clipping.page is not None else '',
+                location=' location ' + clipping.location if clipping.location is not None else '',
+                added=clipping.added,
+            )
+        elif field == 'Source':
+            yield '{kind} from {document}{page}{location}'.format(
+                kind=clipping.kind,
+                document=clipping.document,
+                page=' page ' + clipping.page if clipping.page is not None else '',
+                location=' location ' + clipping.location if clipping.location is not None else '',
+            )
+        else:
+            raise ValueError('Unknown field: ' + field)
 
 
 main()
---
+++ kindle_highlights/config.md
@@ -0,0 +1,3 @@
+`last_added` is the time of the last highlight which was added to Anki.
+Highlights from before this time will not be re-added.
+Click on the Restore Defaults button to set it to null and add all highlights again.
